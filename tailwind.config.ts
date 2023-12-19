1229cd08e6aabdbfc87638c6e168ccaae01fb13e
lokeshdhakar97 <lokeshdhakar6633@gmail.com>
2023-12-20 01:29:58 +0530

    style: add colors to Tailwind config and default styles to global CSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,12 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        manatee: '#999FAA',
+        'bright-gray': '#E8EAEE',
+        'raspberry-pink': '#EB518F',
+        'safety-yellow': '#F4CF08',
+      },
     },
   },
   plugins: [],
