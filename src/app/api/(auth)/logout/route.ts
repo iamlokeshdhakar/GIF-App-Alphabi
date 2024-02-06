74b55f1d324921297e25c33f3d56d7e23b855051
lokeshdhakar97 <lokeshdhakar6633@gmail.com>
2024-02-06 10:35:48 +0530

    resolve handler error

--- src/app/api/(auth)/logout/route.ts
+++ src/app/api/(auth)/logout/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-export default async function handler(req: NextRequest, res: NextResponse) {
+export default async function GET(req: NextRequest, res: NextResponse) {
   req.cookies.clear()
   return Response.json({ message: 'User logged out successfully' })
 }
