c4d4d23262efbb87ff5752f562f30c0adaf359a2
lokeshdhakar97 <lokeshdhakar6633@gmail.com>
2024-02-06 10:46:42 +0530

    deployed error resolve try 4

--- src/app/api/(auth)/logout/route.ts
+++ src/app/api/(auth)/logout/route.ts
@@ -1,6 +1,12 @@
 // import { NextRequest, NextResponse } from 'next/server'
 
+import { NextRequest, NextResponse } from 'next/server'
+
 // export async function GET(req: NextRequest, res: NextResponse) {
 //   req.cookies.clear()
 //   return Response.json({ message: 'User logged out successfully' })
 // }
+
+export async function GET(req: NextRequest) {
+  return Response.json({ message: 'User logged out successfully' })
+}
